799ed2502e9a593d184421ab6bff6c22e41958a4
Alex-Fredy <barcelona_216@hotmail.com>
2022-04-22 14:25:14 -0500

    Add BookController.php changes

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -4,18 +4,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
   
     public function index()
     {
-        //
+        $books = Book::all();
+        return $books;
     }
 
    public function store(Request $request)
     {
-        //
+        
     }
 
    public function show($id)
